dffd669f4d91817bd6d60b481faf6155965970f6
Matveev Timofey <justshowel@gmail.com>
2022-05-31 03:46:52 +0300

    refactor: Rewrite css selectors by bem guidelines. Change html and js accordingly. This change fulfills rule 2.1 from html-and-css-extended.md and rules 3.1-3.4 from html-and-css.md.

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.className='todo-list__todo-item';
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -33,18 +34,21 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    // .task was used earlier
+    label.className='todo-list__todo-item-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className='todo-list__todo-item-checkbox';
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="todo-list__todo-item-input item-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button button--edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button button--delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='button__image';
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -84,8 +88,8 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var editBtn=listItem.querySelector(".button.button--edit");
+    var containsClass=listItem.classList.contains("todo-list__todo-item--editable");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +103,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("todo-list__todo-item--editable");
 };
 
 
@@ -155,9 +159,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var checkBox=taskListItem.querySelector(".todo-list__todo-item-checkbox");
+    var editButton=taskListItem.querySelector(".button.button--edit");
+    var deleteButton=taskListItem.querySelector(".button.button--delete");
 
 
     //Bind editTask to edit button.
