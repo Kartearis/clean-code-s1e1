8ad3161f296bdb6aac118ac85ef284c82d86c0bd
Matveev Timofey <justshowel@gmail.com>
2022-05-31 17:31:50 +0300

    fix: Images on delete buttons now have correct alt values

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className='button__image';
+    deleteButtonImg.alt = 'Delete item';
     deleteButton.appendChild(deleteButtonImg);
 
 
